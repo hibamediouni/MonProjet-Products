cf491fb9568bdd4c160f6aed90b72eb06beb37a9
Hiba Mediouni <hibamediouniif5@gmail.com>
2018-04-17 14:10:10 +0100

    Refarcoristation en utilisant service @injectable

--- src/app/services/products-list.service.ts
+++ src/app/services/products-list.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../domain/product';
 
 @Injectable()
 export class ProductsListService {
-
+   data : Product[] =  [ {code: 'P1000', nom: 'Café', prixUnitaire: 1500.5},
+   {code: 'P2000', nom: 'the', prixUnitaire: 1600.6},
+   {code: 'P3000', nom: 'sucre', prixUnitaire: 1700.5}
+ ];
   constructor() { }
+  getAllProducts(): Product[] {
+    return this.data;
+  }
 
 }
